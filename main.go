8938df1ce51f7f489e68158fc485b10a7168f559
cameronmukherjee <s5111158@bournemouth.ac.uk>
2020-04-27 20:59:19 +0100

    exporting to JSON now works

--- main.go
+++ main.go
@@ -9,32 +9,27 @@ import (
 	"./getos"
 )
 
-// Final struct of all other structs
-type Final struct {
-	user    getos.User          `Json:"User"`
-	cpuInfo getos.CPUInfo       `Json:"CPU Info"`
-	osInfo  getos.OS            `Json:"OS"`
-	uptime  getos.Uptime        `Json:"Uptime"`
-	virtMem getos.VirtualMemory `Json:"Virtual Memory"`
-	getIPs  getinternet.IP      `Json:"IP Addresses"`
+// Export struct of all other structs
+type Export struct {
+	User    getos.User          `Json:"User"`
+	CPUInfo getos.CPUInfo       `Json:"CPU Info"`
+	OSInfo  getos.OS            `Json:"OS"`
+	Uptime  getos.Uptime        `Json:"Uptime"`
+	VirtMem getos.VirtualMemory `Json:"Virtual Memory"`
+	GetIPs  getinternet.IP      `Json:"IP Addresses"`
 }
 
 func main() {
-	// postObject := Final{
-	// 	user:    getos.GetUserDetails(),
-	// 	cpuInfo: getos.GetProcessorDetails(),
-	// 	osInfo:  getos.GetOS(),
-	// 	uptime:  getos.GetUptime(),
-	// 	virtMem: getos.GetVirtualMemUsage(),
-	// 	getIPs:  getinternet.GetIPs(),
-	// }
-	// jsonAddToFile(postObject)
-	jsonAddToFile(getos.GetUserDetails())
-	jsonAddToFile(getos.GetProcessorDetails())
-	jsonAddToFile(getos.GetOS())
-	jsonAddToFile(getos.GetUptime())
-	jsonAddToFile(getos.GetVirtualMemUsage())
-	jsonAddToFile(getinternet.GetIPs())
+	postObject := Export{
+		User:    getos.GetUserDetails(),
+		CPUInfo: getos.GetProcessorDetails(),
+		OSInfo:  getos.GetOS(),
+		Uptime:  getos.GetUptime(),
+		VirtMem: getos.GetVirtualMemUsage(),
+		GetIPs:  getinternet.GetIPs(),
+	}
+	// fmt.Printf("%+v\n", postObject)
+	jsonAddToFile(postObject)
 }
 
 func jsonAddToFile(input interface{}) {
@@ -44,7 +39,9 @@ func jsonAddToFile(input interface{}) {
 		log.Fatal(err)
 	}
 	data := string(jsonData)
-	data = data + "," + "\n"
+	// data = data + "," + "\n"
+	data = data + ","
+	// f, err := os.OpenFile("output.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	f, err := os.OpenFile("output.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
